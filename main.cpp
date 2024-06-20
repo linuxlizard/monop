2509b397d50c66a4bad955b56563d23d240fd6ca
David Poole <testcluster@gmail.com>
2024-06-20 15:30:05 -0700

    add Go; add player moving

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 // davep 20240615 I'm bored. Let's play with C++ and write a monopoly simulator.
 
+// After having played with Rust for a while, can I do C++ without any dynamic memory?
+
+
 #include <iostream>
 #include <fstream>
 #include <random>
@@ -10,18 +13,67 @@
 #include <string_view>
 #include <utility>
 #include <fmt/format.h>
+#include <cassert>
+
+// spaces on the monopoly board
+const uint BOARD_SIZE = 40;
+
+// clion AI Assistant
+// FIXME probably don't need to initialize all these every time
+int roll_die()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> distr(1, 6);
+	return distr(gen);
+}
 
 class Player
 {
 public:
-	explicit Player(uint cash) : cash{cash} {};
+	explicit Player(uint cash) : cash{cash}, position{0}, _passed_go{false} {};
 
-public:
+	std::pair<uint,uint> roll() {
+		die1 = roll_die();
+		die2 = roll_die();
+		return std::make_pair(die1,die2);
+	}
+
+	uint move() {
+		position += die1 + die2;
+
+		if (position > BOARD_SIZE) {
+			_passed_go = true;
+			position = position % BOARD_SIZE;
+		}
+		else {
+			_passed_go = false;
+		}
+
+		return position;
+	}
+
+	void add_money(uint moola) { cash += moola; }
+
+	uint where() { return position; }
+
+	bool passed_go() {
+		return _passed_go;
+	}
+
 	[[nodiscard]] uint getCash() const {
 		return cash;
 	}
+
 private:
 	uint cash;
+
+	// where I am on the board (index)
+	uint position;
+
+	uint die1, die2;
+
+	bool _passed_go;
 };
 
 enum Rent
@@ -40,6 +92,14 @@ class Card
 public:
 	explicit Card(std::string name) : name{std::move(name)} {};
 	explicit Card(const char *name) : name(name) {};
+
+	const std::string name;
+};
+
+class GoCard
+{
+public:
+	explicit GoCard(std::string name) : name{std::move(name)} {};
 
 	const std::string name;
 };
@@ -64,6 +124,11 @@ private:
 Property::Property(std::string  name, std::vector<uint> values)
 	: name(std::move(name))
 {
+	if (values.size() != 9) {
+		std::string msg = fmt::format("expected 9 values but found {}", values.size());
+		throw std::runtime_error(msg);
+	}
+
 	this->purchase_price = values[0];
 	this->house_cost = values[1];
 	std::copy(values.begin() + 2, values.end(), this->rents.begin());
@@ -104,15 +169,7 @@ public:
 	const std::string name;
 };
 
-using Space = std::variant<Property, CommunityChest, Chance, Penalty, Railroad, Utility>;
-
-// clion AI Assistant
-int roll_die() {
-	std::random_device rd;
-	std::mt19937 gen(rd());
-	std::uniform_int_distribution<> distr(1, 6);
-	return distr(gen);
-}
+using Space = std::variant<GoCard, Property, CommunityChest, Chance, Penalty, Railroad, Utility>;
 
 Space parse_board_line(const std::string &line) {
 	// https://www.youtube.com/watch?v=V14xGZAyVKI
@@ -182,19 +239,12 @@ Space parse_board_line(const std::string &line) {
 		return Utility(tokens[0]);
 	}
 
+	if (tokens[0] == "Go") {
+		return GoCard(tokens[0]);
+	}
+
 	auto msg = fmt::format("unidentified card \"{}\"", tokens[0]);
 	throw std::runtime_error(msg);
-}
-
-void parse_board_line2(const std::string &line)
-{
-	std::istringstream v { line };
-	std::vector<std::string> tokens;
-	std::string token;
-	while (std::getline(v, token, ',')) {
-		tokens.push_back(token);
-	}
-	std::cout << "found " << tokens.size() << " tokens\n";
 }
 
 std::vector<Space> load_board(const std::string &file_name)
@@ -235,26 +285,29 @@ int main() {
 	const std::string board_filename { "/home/dpoole/src/monop/board.txt" };
 	std::vector<Space> board = load_board(board_filename);
 
+	assert(board.size() == BOARD_SIZE && "The board must contain 40 elements.");
 
 	Player p1 { 1500 };
 	Player p2 { 1500 };
 
 	for (int i=0 ; i<10 ; i++ ) {
-		int die1 = roll_die();
-		int die2 = roll_die();
-		fmt::print("You rolled {} {}\n", die1, die2);
+		auto roll = p1.roll();
 
-		int sum = die1 + die2;
-		auto land = board.at(sum);
+		fmt::print("You rolled {} {}\n", roll.first, roll.second);
+
+		uint pos = p1.move();
+
+		auto land = board.at(pos);
+
 		//std::visit(GetName{}, land);
 		std::visit([](auto&& arg){ fmt::print("{}\n",arg.name); }, land);
 
+		if (p1.passed_go()) {
+			fmt::print("player has passed Go!\n");
+			p1.add_money(200);
+		}
+
 	}
-
-//	board.emplace_back( Property(60, "Mediterranean Avenue") );
-//	board.emplace_back( CommunityChest() );
-//	board.emplace_back( Property(60, "Baltic Avenue") );
-
 
 	return 0;
 }
